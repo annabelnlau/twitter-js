1d67d30524845cd3c20399543df34b30c1544344
Nunny R <nunnyreyes@gmail.com>
2017-11-07 12:40:31 -0500

    Welcome to Twitter homepage

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-const express = require('express');
+const express = require('express'); 
 const app = express(); 
 const nunjucks = require('nunjucks');
 const chalk = require('chalk');
 const prompt = chalk.green('\nprompt >');
+const routes = require('./routes')
+app.use('/', routes);
 
 app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
 nunjucks.configure('views'); // point nunjucks to the proper directory for templates
+
+app.use(express.static('public'))
 
 app.use(function(req,res,next) {
     //setTimeout(function() {
@@ -22,19 +26,19 @@ app.listen(3000, function(){
   })
 
 
-app.get('/', function(request, response, next){
-    //response.send('<h1>Welcome to the fiesta!!!!!!!</h1>')
-    const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
-    response.render( 'index', {title: 'Hall of Shame', people: people} );
-  })
+// app.get('/', function(request, response, next){
+//     //response.send('<h1>Welcome to the fiesta!!!!!!!</h1>')
+//     const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+//     response.render( 'index', {title: 'Hall of Shame', people: people} );
+//   })
 
-app.get('/news', function(request, response, next){
-    response.send('<h1>News!!!!!</h1>')
-    next(); //was next properly invoked here
-  })
+// app.get('/news', function(request, response, next){
+//     response.send('<h1>News!!!!!</h1>')
+//     next(); //was next properly invoked here
+//   })
 
-  app.get('/special/', function(request, response, next){
-    response.send('<h1>you reached the special area</h1>')
-    next();
-  })
+//   app.get('/special/', function(request, response, next){
+//     response.send('<h1>you reached the special area</h1>')
+//     next();
+//   })
 
